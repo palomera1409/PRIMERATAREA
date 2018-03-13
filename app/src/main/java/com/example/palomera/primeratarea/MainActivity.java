c8141b60db1eeab86ab765b548be110d428f05df
palomera <chore9520@hotmail.com>
2018-03-13 10:55:00 -0600

    Initial commit

---
+++ app/src/main/java/com/example/palomera/primeratarea/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.palomera.primeratarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ //declaracion de variables
+
+    EditText texto;
+    TextView respuesta;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto= (EditText)findViewById(R.id.editText);
+        respuesta=(TextView)findViewById(R.id.textView2);
+
+    }
+
+public void  cambiar (View v)
+    {
+        String cadena= texto.getText().toString();
+       String cadenai = "";
+
+        for( int x =cadena.length()-1; x>=0; x--)
+        {
+
+            cadenai = cadenai + cadena.charAt(x);
+
+
+        }
+
+        respuesta.setText(cadenai);
+
+
+
+    }
+
+
+
+}
